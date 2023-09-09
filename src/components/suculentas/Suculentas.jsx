0f5709d4394aae17037960530de157fe50064a20
danielay <danielitayero@gmail.com>
2023-09-08 22:21:04 -0400

    feat: adjut suculentas component to comunicate with back end

--- src/components/suculentas/Suculentas.jsx
+++ src/components/suculentas/Suculentas.jsx
@@ -1,10 +1,10 @@
-import ProductCard from "../productCard/ProductCard";
+import ProductCard1 from "../productCard1/ProductCard1";
 import "./Suculentas.css";
 import axios from "axios";
 import { useState , useEffect } from "react";
 import { json } from "react-router-dom";
 
-const succUrl = "http://localhost:3001/suculentas" ;
+const succUrl = "http://127.0.0.1:8000/plants/plant/" ;;
 
 
 function Suculentas() {
@@ -15,22 +15,23 @@ function Suculentas() {
         async function handleGet() {
             let res = await axios.get(succUrl) ;
             console.log(res.data)
-            setGetData(res.data)
+            const cactusArrays = res.data.filter((item) => item.category === "Suculentas");
+            console.log(cactusArrays);
+            setGetData(cactusArrays);
         }
         handleGet();
     }, [])
     
 
-    return  <div className = "contcard">
+    return  <div className = "contcard" name= "succ">
     <div className="fancy">
-    
         <div className="headerCactus">
             Succulents
         </div>
         <div className="gridCard">
             {getData.map((item)=> {
                 console.log(item)
-                return <ProductCard name = {item.name} precio = {item.price} img = {item.photo}/>
+                return <ProductCard1 name = {item.name} precio = {item.cost} img = {item.image} id = {item.id}/>
             })}
         </div>
 
