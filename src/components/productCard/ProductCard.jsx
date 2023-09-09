b29f404ecf77f217fa1f8dc6724e9cbf0cee2dc6
danielay <danielitayero@gmail.com>
2023-09-08 22:20:04 -0400

    feat: adjut productCard component to comunicate with back end

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -6,8 +6,9 @@ import axios from "axios";
 import { useState , useEffect } from "react";
 import { json } from "react-router-dom";
 
-const orderUrl = "http://localhost:3001/orderItem"
 
+const orderUrl = "http://localhost:3001/orderItem"
+const qty = 1;
 
 function newOrder(name,price,photo,qty) {
     return {
@@ -22,34 +23,36 @@ function cartNotExist(res) {
     return res.data.length == 0
 }
 
-function createCart(name,price,photo) {
+function createCart(name,price,photo,qty) {
     const order = newOrder(name,price,photo,1);
     axios.post(orderUrl, order);
 }
 
 function ProductCard({name , precio, img}) {
 
-    const addItem = async(name,price,photo) => {
+    
+
+    const addItem = async(name,price,img,qty) => {
         
         let isExisting = false;
         const res = await axios.get(orderUrl);
                 
         if (cartNotExist(res)) {
-            createCart(name,price,photo)
+            createCart(name,price,img,qty)
             return
         }
 
         res.data.map( (orderItem) => {
             if (name === orderItem.name) {
                 orderItem.qty += 1;
-                const order = newOrder(name,price,photo,orderItem.qty)
+                const order = newOrder(name,price,img,orderItem.qty)
                 axios.put(`${orderUrl}/${orderItem.id}`,order);
                 isExisting = true ;
             }               
         });
 
         if (isExisting == false) {                            
-            const order = newOrder(name,price,photo,1)
+            const order = newOrder(name,price,img,qty)
             axios.post(orderUrl,order);
         }
         
@@ -59,7 +62,7 @@ function ProductCard({name , precio, img}) {
         <div className="allCard1">
             <div className="card12">
                 <div className="imgBx1">
-                        <img src={`./${img}.jpeg`}  />
+                        <img src={`http://localhost:8000${img}`}  />
                 </div>
                 <div className="namePlant">
                         {name}
@@ -69,15 +72,12 @@ function ProductCard({name , precio, img}) {
                     <div className="price">
                         <span>${precio}</span>
                     </div>
-                    <div className="add" onClick={() => addItem(name,precio,img)}>
+                    <div className="add" onClick={() => addItem(name,precio,img,qty,img)}>
                         <AiOutlinePlus/>
                     </div>
                 </div>
                
             </div>
-            {/*<div className="mmm">
-                <img src={img1} alt="" />
-    </div>*/}
         </div>
     );
 }
